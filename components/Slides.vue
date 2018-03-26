5a1419b819f7f4c6b4e5e942aca4621f348a6b01
ms <ms@newdev.siduction.org>
2018-03-26 21:42:13 +0530

    Removed unnecessay files

--- components/Slides.vue
+++ components/Slides.vue
@@ -1,38 +1,24 @@
 <template>
 <div class="slide-main-body"> 
-
     <img :src="slide.wpimgsrc.imgsrc" alt="" class="slide-image">
-
     <div class="slide-body">
-
         <h1>{{ slide.title.rendered }}</h1>
-
         <button>Look</button>
-
     </div>
 </div>
 </template>
 
 <script>
 export default {
-    props : ['slide','imgsrcs'],
-    data(){
-        return{
-            imgsrc : "http://powerupboston.com/dev/wp-content/uploads/2016/04/printers02.jpg",
-        }
-    },
-   
+    props : ['slide','imgsrcs']
 };
 </script>
 
 <style scoped>
     .slide-image{
-       /* max-width: 100%; */
        width: 100%;
         height: 800px;       
     }
-
-
 
     .slide-body{
         position: absolute;
